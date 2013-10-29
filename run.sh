5a9073e2e4c129ae44e70f2d76636b985fb3fa11
bluedragon1 <ryu1aoyama@gmail.com>
2013-10-29 16:10:08 +0900

    string type is now available

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$LMNTAL_HOME/installed/bin/slim --use-builtin-rule io/shortestio.lmn io/dump.lmn makedata.lmn $*
+$LMNTAL_HOME/installed/bin/slim --use-builtin-rule io/shortestio.lmn io/dump.lmn makedata.lmn rule/basic_rule.lmn $*
