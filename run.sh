93110755a76f20ca9b87c7bf6fd555ee3f241118
bluedragon1 <ryu1aoyama@gmail.com>
2013-11-05 18:50:29 +0900

    a few modification

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$LMNTAL_HOME/installed/bin/slim --use-builtin-rule io/shortestio.lmn io/dump.lmn makedata.lmn rule/basic_rule.lmn $*
+$LMNTAL_HOME/installed/bin/slim --no-dump --use-builtin-rule io/shortestio.lmn io/dump.lmn makedata.lmn rule/basic_rule.lmn $*
