30831f27b3f75453b7f0ac96eea5d794d9ae2d01
bluedragon1 <ryu1aoyama@gmail.com>
2013-11-11 18:28:54 +0900

    usable random data create

--- script/java_makedata.sh
+++ script/java_makedata.sh
@@ -2,6 +2,6 @@
 cd ..
 echo "$1" > .emake
 echo "$2" >> .emake
-$LMNTAL_HOME/bin/lmntal -s3 example/integer_e_data_make_java.lmn > benchmarks/integer/$1_$2.txt < .emake
+$LMNTAL_HOME/bin/lmntal -s2 example/integer_e_data_make_java.lmn < .emake | sed -e '$d' > benchmarks/integer/$1_$2.txt 
 rm .emake
 cd script
