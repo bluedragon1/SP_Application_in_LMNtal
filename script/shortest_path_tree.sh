c34ac7514868d712bc6ba22f6bc6b86b72ecf294
UedaLab <aoyama@ueda.info.waseda.ac.jp>
2013-11-11 11:05:34 +0900

    a few modification

--- script/shortest_path_tree.sh
+++ script/shortest_path_tree.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ..
-./run.sh example/shortestpath_tree.lmn < benchmarks/$1.txt > benchmarks/$1.dot
+./run.sh example/shortestpath_tree.lmn < script/$1 > benchmarks/$2_tree.dot
 cd script
-dot -Tpng ../benchmarks/$1.dot -o $1.png
-#display $1.png
+dot -Tpng ../benchmarks/$2_tree.dot -o $2_tree.png
+display $2_tree.png
